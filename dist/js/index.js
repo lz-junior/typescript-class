3c280ffc9e96d1b39c8be2da4c554e7f7591f542
lz-junior <correajr.dev@gmail.com>
2024-01-30 22:57:37 -0300

    tipagem

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,3 +2,22 @@
 let x = 10;
 x = 18;
 console.log(x);
+// tipos básicos
+let firstName = "Matheus";
+let age = 23;
+const isAdmin = true;
+// object
+const myNumbers = [1, 2, 3];
+// tuplas
+let myTuple;
+myTuple = [5, "teste", ["a", "b"]];
+// object literals: {prop: value}
+const user = {
+    name: "Pedro",
+    age: 18
+};
+// any
+let a = 0;
+a = "teste";
+a = true;
+a = 39;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,27 @@ let x:number = 10;
 x = 18
 
 console.log(x)
+
+// tipos básicos
+let firstName: string = "Matheus";
+let age: number = 23;
+const isAdmin: boolean = true;
+
+// object
+const myNumbers: number[] = [1, 2, 3];
+
+// tuplas
+let myTuple: [number, string, string[]]
+myTuple = [5, "teste", ["a", "b"]]
+
+// object literals: {prop: value}
+const user: { name: string; age: number } = {
+  name: "Pedro",
+  age: 18
+}
+
+// any
+let a: any = 0;
+a = "teste"
+a = true
+a = 39
