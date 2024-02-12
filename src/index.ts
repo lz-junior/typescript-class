c3f25bf92abb1287f0e1ad6ec723514e21ac476f
lz-junior <correajr.dev@gmail.com>
2024-02-12 14:04:26 -0300

    keyof

--- src/index.ts
+++ src/index.ts
@@ -121,91 +121,129 @@
 // showArrayItems(a1)
 // showArrayItems(a2)
 
+
+
+
+
+// EXEMPLOS
+
+
 // classes
-class User {
-  name
-  role
-  isApproved
+// class User {
+//   name
+//   role
+//   isApproved
 
-  constructor(name:string, role:string, isApproved:boolean) {
-    this.name = name
-    this.role = role
-    this.isApproved = isApproved
-  }
+//   constructor(name:string, role:string, isApproved:boolean) {
+//     this.name = name
+//     this.role = role
+//     this.isApproved = isApproved
+//   }
 
-  showUserName() {
-    console.log(`O nome do usuário é ${this.name}`);
-  }
+//   showUserName() {
+//     console.log(`O nome do usuário é ${this.name}`);
+//   }
 
-  showUserRole(canShow: boolean): void {
-    if(canShow){
-      console.log(`Cargo do usuário é: ${this.role}`);
-      return;
-    }
-    console.log("Informação restrita!")
+//   showUserRole(canShow: boolean): void {
+//     if(canShow){
+//       console.log(`Cargo do usuário é: ${this.role}`);
+//       return;
+//     }
+//     console.log("Informação restrita!")
+//   }
+// }
+
+// const junior = new User("Junior", "dev", true);
+// console.log(junior);
+
+// junior.showUserName();
+// junior.showUserRole(false);
+
+// // interfaces em classes
+// interface IVehicles {
+//   brand: string;
+//   showBrand(): void;
+// }
+
+// class Car implements IVehicles {
+//   brand;
+//   wheels;
+
+//   constructor(brand: string, wheels: number) {
+//     this.brand = brand;
+//     this.wheels = wheels;
+//   }
+//   showBrand(): void {
+//     console.log(`A marca do carro é: ${this.brand}`);
+//   }
+// }
+
+// const fusca = new Car("VW", 4);
+// fusca.showBrand();
+
+// // herança
+// class SuperCar extends Car {
+//   engine;
+
+//   constructor(brand: string, wheels: number, engine: number) {
+//     super(brand, wheels);
+//     this.engine = engine;
+//   }
+// }
+
+// const a4 = new SuperCar("Audi", 4, 2.0);
+
+// console.log(a4);
+// a4.showBrand();
+
+// // decorators
+// function BaseParameters() {
+//   return function <T extends { new(...args: any[]): {} }>(constructor: T) {
+//     return class extends constructor {
+//       id = Math.random();
+//       createdAt = new Date();
+//     };
+//   };
+// }
+
+// @BaseParameters()
+// class Person {
+//   name;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// const pedro = new Person("Pedro");
+// console.log(pedro);
+
+
+
+
+type Address = {
+  street: string
+  num: number
+}
+
+type User = {
+  name?: string
+  age: number
+  address: Address
+}
+
+type UserProperties = keyof User
+
+function pickProperty(user: User, property: UserProperties) {
+  return user[property]
+}
+
+const usuario: User = {
+  name: 'Carlos',
+  age: 27,
+  address: {
+    street: 'Rua dos Camargos',
+    num: 239
   }
 }
 
-const junior = new User("Junior", "dev", true);
-console.log(junior);
-
-junior.showUserName();
-junior.showUserRole(false);
-
-// interfaces em classes
-interface IVehicles {
-  brand: string;
-  showBrand(): void;
-}
-
-class Car implements IVehicles {
-  brand;
-  wheels;
-
-  constructor(brand: string, wheels: number) {
-    this.brand = brand;
-    this.wheels = wheels;
-  }
-  showBrand(): void {
-    console.log(`A marca do carro é: ${this.brand}`);
-  }
-}
-
-const fusca = new Car("VW", 4);
-fusca.showBrand();
-
-// herança
-class SuperCar extends Car {
-  engine;
-
-  constructor(brand: string, wheels: number, engine: number) {
-    super(brand, wheels);
-    this.engine = engine;
-  }
-}
-
-const a4 = new SuperCar("Audi", 4, 2.0);
-
-console.log(a4);
-a4.showBrand();
-
-// decorators
-function BaseParameters() {
-  return function <T extends { new(...args: any[]): {} }>(constructor: T) {
-    return class extends constructor {
-      id = Math.random();
-      createdAt = new Date();
-    };
-  };
-}
-
-@BaseParameters()
-class Person {
-  name;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-}
-const pedro = new Person("Pedro");
-console.log(pedro);
+console.log(pickProperty(usuario, 'age'))
